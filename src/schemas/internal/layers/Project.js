1c9ef8d3393b5fc6b1468d7f28f3dfe5a4f5e434
idnc_sk <me@idnc.sk>
2025-11-09 23:11:13 +0100

    uncertain the future of this module is

---
+++ src/schemas/internal/layers/Project.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import Layer from './BaseLayer.js';
+
+export default class Project extends Layer {
+
+    constructor(name, options = {}) {
+        super(name, options);
+    }
+
+}
